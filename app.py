049c034257eead96b8a7a4ebe9501bef8073430d
Hrithik-Vashishtha <hrithikvashishtha006@gmail.com>
2025-05-25 12:05:32 +0530

    added demo

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask CI/CD!"
 
+@app.route('/demo')
+def demo():
+    return "A demo endpoint"
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
